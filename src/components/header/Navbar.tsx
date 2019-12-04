91527635652552ce0cbaede649a5a160bbe3e57d
Henry Fellerhoff <henryfellerhoff@gmail.com>
2019-12-03 23:11:50 -0600

    Quick padding fix

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import logo from '../../assets/logo.png';
 import './Navbar.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 interface Props {}
 
@@ -21,17 +21,17 @@ const Navbar: React.FC<Props> = () => {
       className='ipa__navbar'
       ref={div => setNavbarRef(div ? div : navbarRef)}
     >
-      <Link className='ipa__navbar__logo-container' to='/'>
+      <NavLink className='ipa__navbar__logo-container' to='/'>
         <img src={logo} alt='Open IPA' className='ipa__navbar__logo' />
         <h5 className='ipa__navbar__title'>Open IPA</h5>
-      </Link>
+      </NavLink>
       <div className='ipa__navbar__link-container'>
-        <Link
+        <NavLink
           className='ipa__navbar__link ipa__navbar__link--featured'
           to='/support'
         >
           Support
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
